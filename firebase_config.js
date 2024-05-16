c721cd53eccbb3368c67793b45f9dabab44919c4
eungobs <eungobs@gmail.com>
2024-05-16 15:44:17 +0200

    Create firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey:"AIzaSyB_iLLcG6QXxvs1rPlbeYez_XfbwJhmWwQ",
+    authDomain: "womens-community-20c6a.firebaseapp.com",
+    projectId: "womens-community-20c6a",
+    storageBucket: "womens-community-20c6a.appspot.com",
+    messagingSenderId: "961442979327",
+    appId: "1:961442979327:web:ab5a1e6166e0a266f65bca"
+};
+
+// Export the Firebase configuration
+export default firebaseConfig;
